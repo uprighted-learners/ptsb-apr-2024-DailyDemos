2d48a5e66f82294c3523f32a720c1d11f52cd333
Tyler Lemke <tyleris@gmail.com>
2024-07-11 17:00:45 -0600

    adding react review starter

---
+++ Week14/thur-react-review/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header name="Grand Master Squeaks"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week14/thur-react-review/src/Header.js
@@ -0,0 +1,8 @@
+export default function Header(props){
+    return (
+        <header>
+            <h1>Sewer Dwelling Reptile Practitioners of Martial Arts</h1>
+            <h2>As presented by {props.name}</h2>
+        </header>
+    )
+}
