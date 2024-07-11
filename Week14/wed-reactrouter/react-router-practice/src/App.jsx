41c30a74bc003c6778e7d591d39cb201bc508049
Tyler Lemke <tlemke@uprighted.com>
2024-07-10 17:17:40 -0700

    updating react router example

--- Week14/wed-reactrouter/react-router-practice/src/App.jsx
+++ Week14/wed-reactrouter/react-router-practice/src/App.jsx
@@ -2,13 +2,34 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
